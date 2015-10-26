b6e029bd19751ed7cc7e32102b37fdb5548d42a1
Sangjin Han <sangjin@eecs.berkeley.edu>
2015-10-25 21:08:59 -0700

    Add RandomUpdate and Rewrite modules

---
+++ core/modules/random_update.c
@@ -0,0 +1,170 @@
+#include "../module.h"
+#include "../utils/random.h"
+
+#define MAX_VARS		16
+
+struct rupdate_priv {
+	int num_vars;
+	struct var {
+		uint32_t mask;
+		uint32_t min;
+		uint32_t range;		/* == max - min + 1 */
+		int16_t offset;
+	} vars[MAX_VARS];
+
+	uint64_t seed;
+};
+
+static struct snobj *rupdate_query(struct module *, struct snobj *);
+
+static struct snobj *rupdate_init(struct module *m, struct snobj *arg)
+{
+	struct rupdate_priv *priv = get_priv(m);
+
+	priv->seed = 1;
+
+	if (arg)
+		return rupdate_query(m, arg);
+	
+	return NULL;
+}
+
+static struct snobj *handle_vars(struct rupdate_priv *priv, 
+		struct snobj *vars)
+{
+	if (snobj_type(vars) != TYPE_LIST)
+		return snobj_err(EINVAL, "'vars' must be a list of maps");
+
+	if (vars->size > MAX_VARS)
+		return snobj_err(EINVAL, "Max %d variables " \
+				"can be specified", MAX_VARS);
+
+	priv->num_vars = 0;
+
+	for (int i = 0; i < vars->size; i++) {
+		struct snobj *var = snobj_list_get(vars, i);
+
+		uint8_t size;
+		int16_t offset;
+		uint32_t mask;
+		uint32_t min;
+		uint32_t max;
+
+		if (var->type != TYPE_MAP)
+			return snobj_err(EINVAL, 
+					"'vars' must be a list of maps");
+
+		offset = snobj_eval_int(var, "offset");
+		size = snobj_eval_uint(var, "size");
+		min = snobj_eval_uint(var, "min");
+		max = snobj_eval_uint(var, "max");
+
+		if (offset < 0)
+			return snobj_err(EINVAL, "Too small 'offset'");
+
+		switch (size) {
+		case 1:
+			offset -= 3;
+			mask = rte_cpu_to_be_32(0xffffff00);
+			min = MIN(min, 0xff);
+			max = MIN(max, 0xff);
+			break;
+
+		case 2:
+			offset -= 2;
+			mask = rte_cpu_to_be_32(0xffff0000);
+			min = MIN(min, 0xffff);
+			max = MIN(max, 0xffff);
+			break;
+
+		case 4:
+			mask = rte_cpu_to_be_32(0x00000000);
+			min = MIN(min, 0xffffffff);
+			max = MIN(max, 0xffffffff);
+			break;
+
+		default:
+			return snobj_err(EINVAL, "'size' must be 1, 2, or 4");
+		}
+
+		if (offset + 4 > MAX_PFRAME)
+			return snobj_err(EINVAL, "Too large 'offset'");
+
+		if (min > max)
+			return snobj_err(EINVAL, "'min' should not be " \
+					"greater than 'max'");
+
+		priv->vars[i].offset = offset;
+		priv->vars[i].mask = mask;
+		priv->vars[i].min = min;
+
+		/* avoid modulo 0 */
+		priv->vars[i].range = (max - min + 1) ? : 0xffffffff;
+	}
+
+	priv->num_vars = vars->size;
+
+	return NULL;
+}
+
+static struct snobj *rupdate_query(struct module *m, struct snobj *q)
+{
+	struct rupdate_priv *priv = get_priv(m);
+
+	struct snobj *vars = snobj_eval(q, "vars");
+
+	struct snobj *err;
+
+	if (vars) {
+		err = handle_vars(priv, vars);
+		if (err)
+			return err;
+	}
+
+	return NULL;
+}
+
+static void rupdate_process_batch(struct module *m, struct pkt_batch *batch)
+{
+	struct rupdate_priv *priv = get_priv(m);
+
+	uint64_t seed = priv->seed;
+	int cnt = batch->cnt;
+
+	for (int i = 0; i < priv->num_vars; i++) {
+		const struct var *var = &priv->vars[i];
+
+		uint32_t mask = var->mask;
+		uint32_t min = var->min;
+		uint32_t range = var->range;
+		int16_t offset = var->offset;
+			
+		for (int j = 0; j < cnt; j++) {
+			struct snbuf *snb = batch->pkts[j];
+			char *head = snb_head_data(snb);
+
+			uint32_t * restrict p;
+			uint32_t rand_val;
+
+			p = (uint32_t *)(head + offset);
+			rand_val = min + rand_fast_range(&seed, range);
+			
+			*p = (*p & mask) | rte_cpu_to_be_32(rand_val);
+		}
+	}
+
+	priv->seed = seed;
+
+	run_next_module(m, batch);
+}
+
+static const struct mclass rupdate = {
+	.name 			= "RandomUpdate",
+	.def_module_name	= "rupdate",
+	.priv_size		= sizeof(struct rupdate_priv),
+	.init 			= rupdate_init,
+	.query			= rupdate_query,
+	.process_batch 		= rupdate_process_batch,
+};
+
+ADD_MCLASS(rupdate)
---
+++ core/modules/rewrite.c
@@ -0,0 +1,120 @@
+#include "../module.h"
+
+#define TEMPLATE_SLOTS		(MAX_PKT_BURST * 2 - 1)
+#define MAX_TEMPLATE_SIZE	1536
+
+struct rewrite_priv {
+	/* For fair round robin we remember the next index for later.
+	 * [0, num_templates - 1] */
+	int next_turn;
+
+	int num_templates;
+	uint16_t template_size[TEMPLATE_SLOTS];
+	unsigned char templates[TEMPLATE_SLOTS][MAX_TEMPLATE_SIZE] __ymm_aligned;
+};
+
+static struct snobj *query(struct module *, struct snobj *);
+
+static struct snobj *init(struct module *m, struct snobj *arg)
+{
+	if (arg)
+		return query(m, arg);
+	
+	return NULL;
+}
+
+/* TODO: make this atomic */
+
+static struct snobj *handle_templates(struct rewrite_priv *priv, 
+		struct snobj *templates)
+{
+	int i;
+
+	if (snobj_type(templates) != TYPE_LIST)
+		return snobj_err(EINVAL, "'templates' must be a list");
+
+	if (templates->size > MAX_PKT_BURST)
+		return snobj_err(EINVAL, "Max %d packet templates " \
+				"can be specified", MAX_PKT_BURST);
+
+	priv->next_turn = 0;
+	priv->num_templates = 0;
+
+	for (i = 0; i < templates->size; i++) {
+		struct snobj *template = snobj_list_get(templates, i);
+
+		if (template->type != TYPE_BLOB)
+			return snobj_err(EINVAL, "Packet template " \
+					"should be BLOB type");
+
+		if (template->size > MAX_TEMPLATE_SIZE)
+			return snobj_err(EINVAL, "Template is too big");
+
+		memset(priv->templates[i], 0, MAX_TEMPLATE_SIZE);
+		memcpy(priv->templates[i], snobj_blob_get(template), 
+				template->size);
+		priv->template_size[i] = template->size;
+	}
+
+	for (i = templates->size; i < TEMPLATE_SLOTS; i++) {
+		int j = i % templates->size;
+		memcpy(priv->templates[i], priv->templates[j], 
+				priv->template_size[j]);
+		priv->template_size[i] = priv->template_size[j];
+	}
+
+	priv->num_templates = templates->size;
+
+	return NULL;
+}
+
+static struct snobj *query(struct module *m, struct snobj *q)
+{
+	struct rewrite_priv *priv = get_priv(m);
+
+	struct snobj *templates = snobj_eval(q, "templates");
+
+	struct snobj *err;
+
+	if (templates) {
+		err = handle_templates(priv, templates);
+		if (err)
+			return err;
+	}
+
+	return NULL;
+}
+
+static void process_batch(struct module *m, struct pkt_batch *batch)
+{
+	struct rewrite_priv *priv = get_priv(m);
+
+	if (priv->num_templates) {
+		int start = priv->next_turn;
+		const int cnt = batch->cnt;
+
+		for (int i = 0; i < cnt; i++) {
+			struct snbuf *snb = batch->pkts[i];
+			char *ptr = snb_head_data(snb);
+			uint16_t size = priv->template_size[start + i];
+
+			snb->mbuf.pkt_len = snb->mbuf.data_len = size;
+			rte_memcpy(ptr, priv->templates[start + i], size);
+		}
+
+		priv->next_turn = (start + cnt) % priv->num_templates;
+	}
+
+	run_next_module(m, batch);
+}
+
+static const struct mclass rewrite = {
+	.name 			= "Rewrite",
+	.priv_size		= sizeof(struct rewrite_priv),
+	.init 			= init,
+	.query			= query,
+	.process_batch 		= process_batch,
+};
+
+ADD_MCLASS(rewrite)
+
