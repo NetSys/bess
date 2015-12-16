6e0d401079e71bbc4c5eaa933163a323cc265f1e
Sangjin Han <sangjin@eecs.berkeley.edu>
2015-12-15 21:02:57 -0800

    add batch_copy()

--- core/pktbatch.h
+++ core/pktbatch.h
@@ -1,13 +1,15 @@
 #ifndef _PKTBATCH_H_
 #define _PKTBATCH_H_
 
+#include <string.h>
+
 #define MAX_PKT_BURST			32
 
 struct snbuf;
 
 struct pkt_batch {
-	struct snbuf * restrict pkts[MAX_PKT_BURST];
 	int cnt;
+	struct snbuf * restrict pkts[MAX_PKT_BURST];
 };
 
 static inline void batch_clear(struct pkt_batch *batch)
@@ -23,6 +25,16 @@ static inline void batch_add(struct pkt_batch *batch, struct snbuf *snb)
 static inline int batch_full(struct pkt_batch *batch)
 {
 	return (batch->cnt == MAX_PKT_BURST);
+}
+
+static inline void batch_copy(struct pkt_batch *dst, 
+		const struct pkt_batch *src)
+{
+	int src_cnt = src->cnt;
+
+	dst->cnt = src_cnt;;
+	memcpy((void *)dst->pkts, (void *)src->pkts, 
+			sizeof(struct snbuf *) * src_cnt);
 }
 
 #endif
