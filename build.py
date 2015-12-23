9f4f4d4e708b742657b2a26c50dfb78293ff19a5
Sangjin Han <sangjin@eecs.berkeley.edu>
2015-12-22 18:51:21 -0800

    adopt DPDK 2.2 with MLX/BNX PMD support

--- build.py
+++ build.py
@@ -6,18 +6,24 @@ import os.path
 import time
 import urllib
 import subprocess
+import textwrap
 
 BESS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DEPS_DIR = '%s/deps' % BESS_DIR 
 
 DPDK_REPO = 'http://dpdk.org/browse/dpdk/snapshot'
-DPDK_VER = 'dpdk-2.0.0'
+DPDK_VER = 'dpdk-2.2.0'
 
-DPDK_DIR = '%s/dpdk' % DEPS_DIR
+DPDK_DIR = '%s/%s' % (DEPS_DIR, DPDK_VER)
 DPDK_URL = '%s/%s.tar.gz' % (DPDK_REPO, DPDK_VER)
 DPDK_FILE = '%s/%s.tar.gz' % (DEPS_DIR, DPDK_VER)
 DPDK_CFLAGS = '"-g -w"'
+DPDK_ORIG_CONFIG = '%s/config/common_linuxapp' % DPDK_DIR
+DPDK_BASE_CONFIG = '%s/%s_common_linuxapp' % (DEPS_DIR, DPDK_VER)
+DPDK_FINAL_CONFIG = '%s/%s_common_linuxapp_final' % (DEPS_DIR, DPDK_VER)
+
+extra_libs = set()
 
 def download_hook(count, block_size, total_size):
     sys.stdout.write('\x08' + ['-', '\\', '|', '/'][int(time.time() * 3) % 4])
@@ -37,6 +43,84 @@ def cmd(cmd):
         print >> sys.stderr, 'Error has occured running command: %s' % cmd
         sys.exit(proc.returncode)
 
+def cmd_success(cmd):
+    try:
+        subprocess.check_call(cmd, 
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                shell=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+def check_c_header(header_file):
+    test_c_file = '%s/test.c' % DEPS_DIR
+    test_o_file = '%s/test.o' % DEPS_DIR
+
+    src = """
+        #include <%s>
+
+        int main()
+        {
+            return 0;
+        }
+        """ % header_file
+
+    try:
+        with open(test_c_file, 'w') as fp:
+            fp.write(textwrap.dedent(src))
+
+        return cmd_success('gcc -c %s -o %s' % (test_c_file, test_o_file))
+    finally:
+        cmd('rm -f %s %s' % (test_c_file, test_o_file))
+
+def check_c_lib(lib):
+    test_c_file = '%s/test.c' % DEPS_DIR
+    test_e_file = '%s/test' % DEPS_DIR
+
+    src = """
+        int main()
+        {
+            return 0;
+        }
+        """
+
+    try:
+        with open(test_c_file, 'w') as fp:
+            fp.write(textwrap.dedent(src))
+
+        return cmd_success('gcc %s -l%s -o %s' % \
+                (test_c_file, lib, test_e_file))
+    finally:
+        cmd('rm -f %s %s' % (test_c_file, test_e_file))
+
+def required(header_file, lib_name):
+    if not check_c_header(header_file): 
+        print >> sys.stderr, 'Error - #include <%s> failed. ' \
+                'Did you install "%s" package?' % (header_file, lib_name)
+        sys.exit(1)
+
+def check_essential():
+    if not cmd_success('gcc -v'):
+        print >> sys.stderr, 'Error - "gcc" is not available'
+        sys.exit(1)
+
+    if not cmd_success('make -v'):
+        print >> sys.stderr, 'Error - "make" is not available'
+        sys.exit(1)
+
+    required('pcap/pcap.h', 'libpcap-dev')
+
+def set_config(filename, config, new_value):
+    with open(filename) as fp:
+        lines = fp.readlines()
+
+    with open(filename, 'w') as fp:
+        for line in lines:
+            if line.startswith(config + '='):
+                line = '%s=%s\n' % (config, new_value)
+            fp.write(line)
+
 def download_dpdk():
     try:
         print 'Downloading %s ...  ' % DPDK_URL,
@@ -46,7 +130,55 @@ def download_dpdk():
         cmd('rm -f %s' % (DPDK_FILE))
         raise
 
+def check_bnx():
+    if check_c_header('zlib.h') and check_c_lib('z'):
+        global extra_libs
+        extra_libs.add('z')
+    else:
+        print ' - "zlib1g-dev" is not available. ' \
+                'Disabling BNX2X PMD...'
+        set_config(DPDK_FINAL_CONFIG, 'CONFIG_RTE_LIBRTE_BNX2X_PMD', 'n')
+
+def check_mlx():
+    if check_c_header('infiniband/verbs_exp.h'):
+        global extra_libs
+        extra_libs.add('ibverbs')
+        #extra_libs.add('mlx5')
+    else:
+        print ' - "Mellanox OFED" is not available. ' \
+                'Disabling MLX4 and MLX5 PMDs...'
+        if check_c_header('infiniband/verbs.h'):
+            print '   NOTE: "libibverbs-dev" does exist, but it does not ' \
+                    'work with MLX PMDs. Instead download OFED from ' \
+                    'http://www.melloanox.com'
+        set_config(DPDK_FINAL_CONFIG, 'CONFIG_RTE_LIBRTE_MLX4_PMD', 'n')
+        set_config(DPDK_FINAL_CONFIG, 'CONFIG_RTE_LIBRTE_MLX5_PMD', 'n')
+
+def generate_extra_mk():
+    global extra_libs
+   
+    with open('core/extra.mk', 'w') as fp:
+        fp.write('LIBS += %s ' % \
+                ' '.join(map(lambda lib: '-l' + lib, extra_libs)))
+
+def configure_dpdk():
+    try:
+        print 'Configuring DPDK...'
+        cmd('cp -f %s %s' % (DPDK_BASE_CONFIG, DPDK_FINAL_CONFIG))
+
+        check_bnx()
+        check_mlx()
+
+        generate_extra_mk()
+
+        cmd('cp -f %s %s' % (DPDK_FINAL_CONFIG, DPDK_ORIG_CONFIG))
+        cmd('make -C %s config T=x86_64-native-linuxapp-gcc' % DPDK_DIR)
+    finally:
+        cmd('rm -f %s' % DPDK_FINAL_CONFIG)
+
 def setup_dpdk():
+    check_essential()
+
     if not os.path.exists(DPDK_DIR):
         if not os.path.exists(DPDK_FILE):
             download_dpdk()
@@ -60,10 +192,7 @@ def setup_dpdk():
 
     # not configured yet?
     if not os.path.exists('%s/build' % DPDK_DIR):
-        print 'Configuring DPDK...'
-        cmd('cp -f %s/%s_common_linuxapp %s/config/common_linuxapp' % \
-                (DEPS_DIR, DPDK_VER, DPDK_DIR))
-        cmd('make -C %s config T=x86_64-native-linuxapp-gcc' % DPDK_DIR)
+        configure_dpdk()
 
     print 'Building DPDK...'
     nproc = int(subprocess.check_output('nproc'))
@@ -71,11 +200,15 @@ def setup_dpdk():
             (nproc, DPDK_DIR, DPDK_CFLAGS))
 
 def build_bess():
+    check_essential()
+
     print 'Building BESS daemon...'
     cmd('make -C core')
     cmd('ln -f -s ../core/bessd bin/bessd')
 
 def build_kmod():
+    check_essential()
+
     print 'Building BESS Linux kernel module... (optional)'
     try:
         cmd('make -C core/kmod')
@@ -100,7 +233,7 @@ def do_dist_clean():
 
 def print_usage():
     print >> sys.stderr, \
-            'Usage: python %s [all|bess|kmod|clean|dist_clean|help]' % \
+            'Usage: %s [all|bess|kmod|clean|dist_clean|help]' % \
             sys.argv[0]
     sys.exit(2)
 
