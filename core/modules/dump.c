e19b99b08f0ef712c77fc96ee8ea375cb82f5c60
Sangjin Han <sangjin@eecs.berkeley.edu>
2016-05-30 23:58:39 -0700

    add Dump module for debugging

---
+++ core/modules/dump.c
@@ -0,0 +1,77 @@
+#include <rte_hexdump.h>
+
+#include "../module.h"
+
+#define NS_PER_SEC	1000000000ul
+
+static const uint64_t default_interval_ns = 1 * NS_PER_SEC;	/* 1 sec */
+
+struct dump_priv {
+	uint64_t min_interval_ns;
+	uint64_t next_ns;
+};
+
+static struct snobj *
+command_set_interval(struct module *m, const char *cmd, struct snobj *arg)
+{
+	struct dump_priv *priv = get_priv(m);
+
+	if (snobj_type(arg) == TYPE_INT) {
+		int sec = snobj_int_get(arg);
+		if (sec < 0)
+			return snobj_err(EINVAL, "invalid interval");
+		priv->min_interval_ns = sec * NS_PER_SEC;
+	} else if (snobj_type(arg) == TYPE_DOUBLE) {
+		double sec = snobj_double_get(arg);
+		if (sec < 0.0)
+			return snobj_err(EINVAL, "invalid interval");
+		priv->min_interval_ns = (uint64_t)(sec * NS_PER_SEC);
+	} else
+		return snobj_err(EINVAL, "argument must be a number");
+
+	return NULL;
+}
+
+static struct snobj *dump_init(struct module *m, struct snobj *arg)
+{
+	struct dump_priv *priv = get_priv(m);
+
+	priv->min_interval_ns = default_interval_ns;
+	priv->next_ns = ctx.current_tsc;
+
+	if (arg)
+		return command_set_interval(m, NULL, arg);
+	else
+		return NULL;
+}
+
+static void dump_process_batch(struct module *m, struct pkt_batch *batch)
+{
+	struct dump_priv *priv = get_priv(m);
+
+	if (unlikely(ctx.current_ns >= priv->next_ns)) {
+		struct snbuf *pkt = batch->pkts[0];
+
+		snb_dump(stdout, pkt);
+		rte_hexdump(stdout, "Metadata buffer",
+				pkt->_metadata, SNBUF_METADATA);
+		priv->next_ns = ctx.current_ns + priv->min_interval_ns;
+	}
+
+	run_choose_module(m, get_igate(), batch);
+}
+
+static const struct mclass dump = {
+	.name 		= "Dump",
+	.help		= "Dump packet data and metadata attributes",
+	.num_igates	= 1,
+	.num_ogates	= 1,
+	.priv_size	= sizeof(struct dump_priv),
+	.init		= dump_init,
+	.process_batch 	= dump_process_batch,
+	.commands	= {
+		{"set_interval", 	command_set_interval},
+	}
+};
+
+ADD_MCLASS(dump)
