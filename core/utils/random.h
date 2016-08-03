965163dc0b8f665e1bf89d560ead223c127a2968
Sangjin Han <sangjin@eecs.berkeley.edu>
2016-08-03 15:23:34 -0700

    add missing header dependency

--- core/utils/random.h
+++ core/utils/random.h
@@ -1,6 +1,8 @@
 #ifndef __RANDOM_H__
 #define __RANDOM_H__
 
+#include <stdint.h>
+
 static inline uint32_t rand_fast(uint64_t *seed)
 {
 	uint64_t next_seed;
@@ -18,15 +20,15 @@ static inline uint32_t rand_fast_range(uint64_t *seed, uint32_t range)
 		uint64_t i;
 		double d;
 	} tmp;
-	
+
 	next_seed = *seed * 1103515245 + 12345;
 
-	/* 
+	/*
 	 * From the MSB,
 	 * 0: sign
 	 * 1-11: exponent (0x3ff == 0, 0x400 == 1)
 	 * 12-63: mantissa
-	 * The resulting double number is 1.(b0)(b1)...(b47), 
+	 * The resulting double number is 1.(b0)(b1)...(b47),
 	 * where next_seed is (b0)(b1)...(b63).
 	 */
 	tmp.i = (next_seed >> 12) | 0x3ff0000000000000ul;
