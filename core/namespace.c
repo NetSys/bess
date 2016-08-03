4ec8ab5173b13fa1cdcad338e519064e9611f147
Sangjin Han <sangjin@eecs.berkeley.edu>
2016-08-03 15:22:05 -0700

    fix (false positive) build warning on old gcc

--- core/namespace.c
+++ core/namespace.c
@@ -17,13 +17,13 @@
 #define NS_CRC_INIT		0xFFFFFFFF
 
 struct ns_elem {
-	ns_type_t type; 
+	ns_type_t type;
 	char name[SN_NAME_LEN];
 	void *object;
 
-	struct cdlist_item ns_table_bhead; 
-	struct cdlist_item ns_type_iter; 
-	struct cdlist_item ns_all_iter; 
+	struct cdlist_item ns_table_bhead;
+	struct cdlist_item ns_type_iter;
+	struct cdlist_item ns_all_iter;
 };
 
 struct ns_table {
@@ -33,7 +33,7 @@ struct ns_table {
 	struct cdlist_head *ns_elem_bhead;	/* hash table */
 
 	/* per-type iterators */
-	struct cdlist_head ns_elem_type_iter[NS_TYPE_MAX]; 
+	struct cdlist_head ns_elem_type_iter[NS_TYPE_MAX];
 	int iterator_cnt[NS_TYPE_MAX];
 } ht;
 
@@ -50,7 +50,7 @@ static int ns_table_init()
 
 	for (i = 0; i < ht.bucket_size; i++)
 		cdlist_head_init(&ht.ns_elem_bhead[i]);
-	
+
 	for (i = 0; i < NS_TYPE_MAX; i++) {
 		cdlist_head_init(&ht.ns_elem_type_iter[i]);
 		ht.iterator_cnt[i] = 0;
@@ -59,39 +59,39 @@ static int ns_table_init()
 	return 0;
 }
 
-static inline uint32_t ns_get_hash(const char *name) 
+static inline uint32_t ns_get_hash(const char *name)
 {
 	uint32_t hash = rte_hash_crc((void *)name, strlen(name), NS_CRC_INIT);
 
 	return hash % ht.bucket_size;
 }
 
-static inline uint32_t ns_get_hash_with_bsize(const char *name, int new_bsize) 
+static inline uint32_t ns_get_hash_with_bsize(const char *name, int new_bsize)
 {
 	uint32_t hash = rte_hash_crc ((void *)name, strlen(name), NS_CRC_INIT);
 
 	return hash % new_bsize;
 }
 
-static void ns_table_dump() 
+static void ns_table_dump()
 {
 	struct ns_elem *entry;
 	struct cdlist_head *bhead;
-			
+
 	log_debug("== ns_table_dump starts ==\n");
 
 	for (int i = 0; i < ht.bucket_size; i++) {
 		bhead = &ht.ns_elem_bhead[i];
 		cdlist_for_each_entry(entry, bhead, ns_table_bhead) {
-			log_debug("entry '%s' is in bucket %d\n", 
+			log_debug("entry '%s' is in bucket %d\n",
 					entry->name, i);
 		}
 	}
 
-	log_debug("== ns_table_dump ends ==\n"); 
+	log_debug("== ns_table_dump ends ==\n");
 }
 
-static int ns_table_resize(int new_bsize) 
+static int ns_table_resize(int new_bsize)
 {
 	struct ns_elem *entry;
 	struct ns_elem *next;
@@ -106,10 +106,10 @@ static int ns_table_resize(int new_bsize)
 	new_bhead = malloc(new_bsize * sizeof(struct cdlist_head));
 	if (!new_bhead)
 		return -ENOMEM;
-	
+
 	for (int i = 0; i < new_bsize; i++)
 		cdlist_head_init(&new_bhead[i]);
-	
+
 	/* relocate elements */
 	for (int i = 0; i < old_bsize; i++) {
 		bhead = &ht.ns_elem_bhead[i];
@@ -128,7 +128,7 @@ static int ns_table_resize(int new_bsize)
 	return 0;
 }
 
-static int ns_table_rebalance() 
+static int ns_table_rebalance()
 {
 	int new_bucket_size = 0;
 
@@ -143,17 +143,17 @@ static int ns_table_rebalance()
 	if (new_bucket_size == 0 || new_bucket_size >= NS_BUCKET_SIZE_MAX ||
 			new_bucket_size <= NS_BUCKET_SIZE_INIT) {
 		/* no resize necessary */
-		return 0; 
+		return 0;
 	}
 
-	ns_table_resize(new_bucket_size); 
+	ns_table_resize(new_bucket_size);
 
 	return 0;
 }
 
 
 /* return 1 if they are equal, return 0 if they are not equal */
-static inline int ns_is_equal_str(const char *name1, const char *name2) 
+static inline int ns_is_equal_str(const char *name1, const char *name2)
 {
 	return !strncmp(name1, name2, SN_NAME_LEN);
 }
@@ -162,7 +162,7 @@ static inline struct ns_elem *ns_lookup_elem_by_name(const char *name)
 {
 	struct cdlist_head *bhead;
 	struct ns_elem *i,*find_i = NULL;
-	
+
 	uint32_t hash = ns_get_hash(name);
 
 	bhead = &ht.ns_elem_bhead[hash];
@@ -184,10 +184,10 @@ int ns_is_valid_name(const char *name)
 	int name_len = strlen(name);
 	if (name_len == 0 || name_len >= SN_NAME_LEN)
 		return 0;
-	
+
 	// characters should be looks like
 	// [_a-zA-Z][_a-zA-Z0-9]*
-	
+
 	c = name[0];
 	if ((c != '_') && !isalpha(c))
 		return 0;
@@ -201,13 +201,13 @@ int ns_is_valid_name(const char *name)
 	return 1;
 }
 
-int ns_name_exists(const char *name) 
+int ns_name_exists(const char *name)
 {
 	struct ns_elem *elem;
 
 	if (ht.item_count == 0 || !ns_is_valid_name(name))
-		return 0; 
-	
+		return 0;
+
 	elem = ns_lookup_elem_by_name(name);
 	if (elem)
 		return 1;
@@ -221,10 +221,10 @@ void *ns_lookup(ns_type_t type, const char *name)
 
 	assert(type >= 0);
 	assert(type < NS_TYPE_ALL);
-	
+
 	if (ht.item_count == 0 || !ns_is_valid_name(name))
-		return NULL; 
-	
+		return NULL;
+
 	elem = ns_lookup_elem_by_name(name);
 
 	if (elem && (elem->type == type))
@@ -233,7 +233,7 @@ void *ns_lookup(ns_type_t type, const char *name)
 	return NULL;
 }
 
-int ns_insert(ns_type_t type, const char *name, void *object) 
+int ns_insert(ns_type_t type, const char *name, void *object)
 {
 	uint32_t hash;
 	struct cdlist_head *bhead, *ihead;
@@ -246,16 +246,16 @@ int ns_insert(ns_type_t type, const char *name, void *object)
 
 	if (type < 0 || type >= NS_TYPE_ALL)
 		return -EINVAL;
-	
+
 	if (ht.iterator_cnt[type] || ht.iterator_cnt[NS_TYPE_ALL])
-		return -EINVAL; 
-	
+		return -EINVAL;
+
 	if (!ns_is_valid_name(name))
-		return -EINVAL; 
+		return -EINVAL;
 
 	if (ns_name_exists(name))
-		return -EEXIST; 
-	
+		return -EEXIST;
+
 	hash = ns_get_hash(name);
 	assert(hash < ht.bucket_size);
 
@@ -276,10 +276,10 @@ int ns_insert(ns_type_t type, const char *name, void *object)
 
 	ihead = &ht.ns_elem_type_iter[type];
 	cdlist_add_tail(ihead, &i->ns_type_iter);
-	
+
 	ihead = &ht.ns_elem_type_iter[NS_TYPE_ALL];
 	cdlist_add_tail(ihead, &i->ns_all_iter);
-	
+
 	ht.item_count++;
 
 	return 0;
@@ -294,21 +294,21 @@ int ns_remove(const char *name)
 		return ret;
 
 	if (!ns_is_valid_name(name))
-		return -EINVAL; 
-	
+		return -EINVAL;
+
 	elem = ns_lookup_elem_by_name(name);
 	if (!elem)
 		return -ENOENT;
-	
+
 	if (ht.iterator_cnt[elem->type] || ht.iterator_cnt[NS_TYPE_ALL])
-		return -EINVAL; 
+		return -EINVAL;
 
 	cdlist_del(&elem->ns_table_bhead);
 	cdlist_del(&elem->ns_type_iter);
 	cdlist_del(&elem->ns_all_iter);
 
 	free(elem);
-	
+
 	ht.item_count--;
 
 	return 0;
@@ -317,42 +317,40 @@ int ns_remove(const char *name)
 void ns_init_iterator(struct ns_iter *iter, ns_type_t type)
 {
 	struct cdlist_head *ihead;
-	
+
 	assert(type >= 0);
 	assert(type < NS_TYPE_MAX);
-		
-	ht.iterator_cnt[type]++;
-	iter->type = type;
 
-	if (ht.item_count == 0) {
-		iter->next = NULL;
+	ht.iterator_cnt[type]++;
+	*iter = (struct ns_iter){ .type = type };
+
+	if (ht.item_count == 0)
 		return;
-	}
-	
+
 	iter->ns_elem_iter = &ht.ns_elem_type_iter[type];
 	ihead = iter->ns_elem_iter;
 
 	if (cdlist_is_empty(ihead))
 		iter->next = NULL;
-	else 
+	else
 		if (iter->type == NS_TYPE_ALL)
-			iter->next = container_of(ihead->next, 
+			iter->next = container_of(ihead->next,
 						struct ns_elem, ns_all_iter);
-		else 
-			iter->next = container_of(ihead->next, 
+		else
+			iter->next = container_of(ihead->next,
 						struct ns_elem, ns_type_iter);
 }
 
-void ns_release_iterator(struct ns_iter* iter) 
+void ns_release_iterator(struct ns_iter* iter)
 {
 	ht.iterator_cnt[iter->type]--;
 }
 
-void *ns_next(struct ns_iter *iter) 
+void *ns_next(struct ns_iter *iter)
 {
 	struct ns_elem *cur_elem;
 	struct cdlist_head *ihead = iter->ns_elem_iter;
-	
+
 	if (!iter->next)
 		return NULL;
 	cur_elem = iter->next;
@@ -360,25 +358,25 @@ void *ns_next(struct ns_iter *iter)
 	if (iter->type == NS_TYPE_ALL) {
 		if (cur_elem->ns_all_iter.next == (struct cdlist_item *)(ihead))
 			iter->next = NULL;
-		else 
+		else
 			iter->next = container_of(cur_elem->ns_all_iter.next,
 						struct ns_elem, ns_all_iter);
 
 	} else {
 		if (cur_elem->ns_type_iter.next == (struct cdlist_item *)(ihead))
 			iter->next = NULL;
-		else 
-			iter->next = container_of(cur_elem->ns_type_iter.next, 
+		else
+			iter->next = container_of(cur_elem->ns_type_iter.next,
 						struct ns_elem, ns_type_iter);
 	}
 	return cur_elem->object;
 }
 
 /* namespace (ns_table) test */
-void ns_valid_name_test() 
+void ns_valid_name_test()
 {
 	int ret;
-	
+
 	char *name1 = "_Sangjin09";	/* valid */
 	char *name2 = "E2Classifier";	/* valid */
 	char *name3 = "101Source";	/* invalid */
@@ -403,7 +401,7 @@ void ns_valid_name_test()
 	log_info("PASS: ns_valid_name_test\n");
 }
 
-void ns_hashtable_test() 
+void ns_hashtable_test()
 {
 	int ret;
 
@@ -433,7 +431,7 @@ void ns_hashtable_test()
 	assert(ret);
 	ret = ns_name_exists(module1_name);
 	assert(!ret);
-	
+
 	//2. insert and name_exist test
 	ret = ns_insert(NS_TYPE_MODULE, module1_name, &module1_obj);
 	assert(!ret); // no error
@@ -441,7 +439,7 @@ void ns_hashtable_test()
 	assert(ret);
 	ret = ns_name_exists(module1_name);
 	assert(ret);
-	
+
 	//3. insert same elements twice
 	ret = ns_insert(NS_TYPE_DRIVER, driver1_name, &driver1_obj);
 	assert(!ret); // no error
@@ -470,11 +468,11 @@ void ns_hashtable_test()
 	ns_remove(port2_name);
 	ret_obj = ns_lookup(NS_TYPE_PORT, port2_name);
 	assert (!ret_obj);
-	
+
 	log_info("PASS: ns_hastable_test\n");
 }
 
-void ns_iterator_test() 
+void ns_iterator_test()
 {
 	char *module1_name = "Sink";
 	char *module2_name = "E2Calssifier";
@@ -485,9 +483,9 @@ void ns_iterator_test()
 	char *port1_name = "in1";
 	char *port2_name = "in2";
 	char *port3_name = "in3";
-	
+
 	char *driver1_name = "ixgbe";
-	
+
 	typedef struct {
 		int value;
 	} object;
@@ -503,12 +501,12 @@ void ns_iterator_test()
 	object port3_obj;
 
 	object driver1_obj;
-	
+
 	struct ns_iter iter_module;
 	struct ns_iter iter_port;
 	struct ns_iter iter_driver;
 	struct ns_iter iter_all;
-	
+
 	int ret;
 	void* ret_obj;
 	int count;
@@ -548,7 +546,7 @@ void ns_iterator_test()
 	assert(ret_obj == &module2_obj);
 	ret_obj = ns_next(&iter_module);
 	assert(ret_obj == &module3_obj);
-	
+
 	// 3-1. insert during iterator running - same type
 	ret = ns_insert(NS_TYPE_MODULE, module5_name, &module5_obj);
 	assert(ret < 0); // error, cannot insert during iterator of same type
@@ -566,15 +564,15 @@ void ns_iterator_test()
 	// 3-3. lookup during iterator running - possible
 	ret_obj = ns_lookup(NS_TYPE_DRIVER, driver1_name);
 	assert (ret_obj == &driver1_obj);
-	
+
 	// 3-4. remove during iterator running - same type
 	ret = ns_remove(module4_name);
 	assert(ret < 0); // error, cannot insert during iterator of same type
-	
+
 	// 3-5. remove during iterator running - different type
 	ret = ns_remove(driver1_name);
 	assert(!ret); // no error
-	
+
 	ret_obj = ns_next(&iter_module);
 	assert(!ret_obj); // empty iterator, again
 	ns_release_iterator(&iter_module);
@@ -590,7 +588,7 @@ void ns_iterator_test()
 	}
 	assert(count == 3); // total 3 ports
 	ns_release_iterator(&iter_port);
-	
+
 	// 4. all iterator count
 	ns_init_iterator(&iter_all, NS_TYPE_ALL);
 	count = 0;
@@ -606,7 +604,7 @@ void ns_iterator_test()
 	log_info("PASS: ns_iterator_test\n");
 }
 
-void ns_table_resize_test() 
+void ns_table_resize_test()
 {
 	char *module1_name = "Sink";
 	char *module2_name = "E2Calssifier";
@@ -617,7 +615,7 @@ void ns_table_resize_test()
 	char *port1_name = "in1";
 	char *port2_name = "in2";
 	char *port3_name = "in3";
-	
+
 	typedef struct {
 		int value;
 	} object;
@@ -633,7 +631,7 @@ void ns_table_resize_test()
 
 	int ret;
 	void* ret_obj;
-	
+
 	ns_table_init();
 
 	//insert elements
@@ -651,7 +649,7 @@ void ns_table_resize_test()
 	assert(!ret); // no error
 	ret = ns_insert(NS_TYPE_PORT, port3_name, &port3_obj);
 	assert(!ret); // no error
-	
+
 	// resize table
 	ret = ns_table_resize(64);
 	if (ret) {
@@ -659,7 +657,7 @@ void ns_table_resize_test()
 		return;
 	}
 
-	// lookup elements	
+	// lookup elements
 	ret_obj = ns_lookup(NS_TYPE_MODULE, module4_name);
 	assert (ret_obj == &module4_obj);
 	ret_obj = ns_lookup(NS_TYPE_MODULE, module2_name);
@@ -677,15 +675,15 @@ void ns_table_resize_test()
 	assert (ret_obj == &port1_obj);
 	ret_obj = ns_lookup(NS_TYPE_PORT, port2_name);
 	assert (ret_obj == &port2_obj);
-	
+
 	// resize table
 	ret = ns_table_resize(16);
 	if (ret) {
 		log_err("FAIL: ns_table_resize()\n");
 		return;
 	}
-	
-	// lookup elements	
+
+	// lookup elements
 	ret_obj = ns_lookup(NS_TYPE_MODULE, module4_name);
 	assert (ret_obj == &module4_obj);
 	ret_obj = ns_lookup(NS_TYPE_MODULE, module2_name);
@@ -703,6 +701,6 @@ void ns_table_resize_test()
 	assert (ret_obj == &port1_obj);
 	ret_obj = ns_lookup(NS_TYPE_PORT, port2_name);
 	assert (ret_obj == &port2_obj);
-	
+
 	log_info("PASS: ns_table_resize_test\n");
 }
