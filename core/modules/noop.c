ef5f42748fa966f118090d1e8bd4d3f9c0281957
Sangjin Han <sangjin@eecs.berkeley.edu>
2016-08-11 16:07:38 -0700

    add mising task registration

--- core/modules/noop.c
+++ core/modules/noop.c
@@ -1,7 +1,17 @@
 #include "../module.h"
 
-static struct task_result
-noop_run_task(struct module *m, void *arg)
+static struct snobj *noop_init(struct module *m, struct snobj *arg)
+{
+	task_id_t tid;
+
+	tid = register_task(m, NULL);
+	if (tid == INVALID_TASK_ID)
+		return snobj_err(ENOMEM, "Task creation failed");
+
+	return NULL;
+}
+
+static struct task_result noop_run_task(struct module *m, void *arg)
 {
 	struct task_result ret;
 
@@ -18,6 +28,7 @@ static const struct mclass noop = {
 	.help		= "creates a task that does nothing",
 	.num_igates	= 0,
 	.num_ogates	= 0,
+	.init 		= noop_init,
 	.run_task 	= noop_run_task,
 };
 
