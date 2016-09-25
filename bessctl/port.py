0b7d0da3e09379b7f26313710e63f08d5e27da05
Sangjin Han <sangjin@eecs.berkeley.edu>
2016-09-24 17:48:55 -0700

    support driver-specific classes

--- bessctl/port.py
+++ bessctl/port.py
@@ -1,16 +1,25 @@
 class Port(object):
-    def __init__(self, driver = 'PMD', name = None, arg = None, **kwargs):
+    def __init__(self, **kwargs):
         self.name = '<uninitialized>'
-        self.driver = driver
+        self.driver = self.__class__.__name__
 
-        ret = self.bess.create_port(driver, name, 
-                self.choose_arg(arg, kwargs))
+        assert self.driver != 'Port', \
+                "do not instantiate 'Port' directly"
+
+        if 'name' in kwargs:
+            name = kwargs['name']
+            del kwargs['name']
+        else:
+            name = None
+
+        ret = self.bess.create_port(self.driver, name, \
+                self.choose_arg(None, kwargs))
 
         self.name = ret['name']
         #print 'Port %s created' % self
 
     def __str__(self):
-        return '%s::%s' % (self.name, self.driver)
+        return '%s/%s' % (self.name, self.driver)
 
     def get_port_stats(self):
         return self.bess.get_port_stats(self.name)
