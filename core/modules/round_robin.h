e3bf1ce61d0ebe421c2eb7dca87ba3f50bd7804c
Justine Sherry <sherry@nefeli.io>
2016-10-19 18:35:02 +0000

    forgot to add in header file.

---
+++ core/modules/round_robin.h
@@ -0,0 +1,72 @@
+#include "../module.h"
+
+#define MAX_RR_GATES 16384
+
+/*!
+ * The RoundRobin module schedules packets from a single input gate across
+ * multiple output gates according to a (you guessed it) RoundRobin scheduling
+ * algorithm:
+ * https://en.wikipedia.org/wiki/Round-robin_scheduling
+ *
+ * EXPECTS: Input packets in any format
+ * MODIFICATIONS: None
+ *
+ * INPUT GATES: 1
+ * OUTPUT GATES: 1..MAX_GATES
+ *
+ * PARAMETERS:
+ * gates    * the number of output gates for the module
+ * mode     * whether to schedule with per-packet or per-batch granularity.
+ *          * options are "packet" or "batch".
+*/
+class RoundRobin : public Module {
+ public:
+  virtual struct snobj *Init(struct snobj *arg);
+
+  virtual void ProcessBatch(struct pkt_batch *batch);
+
+  static const gate_idx_t kNumIGates = 1;
+  static const gate_idx_t kNumOGates = MAX_GATES;
+
+  static const Commands<RoundRobin> cmds;
+
+ private:
+  /*! 
+   * Switches the RoundRobin module between "batch" vs "packet" scheduling.
+   */
+  struct snobj *CommandSetMode(struct snobj *arg);
+  
+  /*!
+   * Sets the number of output gates.
+   */
+  struct snobj *CommandSetGates(struct snobj *arg);
+
+/*!
+ * TODO: RoundRobin currently does not support multiple workers.
+ */
+  gate_idx_t gates_[MAX_RR_GATES] = {};
+  
+  //The total number of output gates
+  int ngates_ = {};
+  //The next gate to transmit on in the RoundRobin scheduler
+  int current_gate_ = {};
+  //Whether or not to schedule per-packet or per-batch
+  int per_packet_ = {};
+};
+
+const Commands<RoundRobin> RoundRobin::cmds = {
+    {"set_mode", &RoundRobin::CommandSetMode, 0},
+    {"set_gates", &RoundRobin::CommandSetGates, 0},
+};
+
+/*! 
+ * Sanity function: is this gate_idx_t possibly a real gate?
+ * Note that true only indicates that the gate_idx_t is not > MAX_GATES and does
+ * not represent the NULL gate -- true does not indicate that the gate is
+ * actually instantiated and connected to anything.
+ */
+static inline int is_valid_gate(gate_idx_t gate) {
+  return (gate < MAX_GATES || gate == DROP_GATE);
+}
+
+
