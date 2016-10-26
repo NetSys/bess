2af068f468d354a3ead1217c73b072881b524790
Sangjin Han <sangjin@eecs.berkeley.edu>
2016-10-25 19:09:16 -0700

    Add missing dependency packages

--- vagrant/packages.sh
+++ vagrant/packages.sh
@@ -2,8 +2,24 @@
 
 apt-get update
 apt-get upgrade -y
-apt-get install -y build-essential libpcap-dev build-essential
+
+# basics
+apt-get install -y git build-essential libpcap-dev zlib1g-dev
+apt-get install -y gdb gdbserver exuberant-ctags cscope vim emacs
+
+# perftools
 apt-get install -y linux-virtual linux-tools-common linux-tools-generic \
-	linux-image-extra-virtual zlib1g-dev gdb gdbserver python-scapy \
-	libgraph-easy-perl exuberant-ctags cscope vim emacs
-apt-get install -y docker.io
+	linux-image-extra-virtual
+
+# grpc
+apt-get install -y libtool autoconf libgoogle-glog-dev libgtest-dev libunwind8-dev liblzma-dev
+git clone -q -b $(curl -s -L http://grpc.io/release) https://github.com/grpc/grpc /opt/grpc
+cd /opt/grpc
+git submodule update --init
+make
+make install
+cd third_party/protobuf
+make install
+
+# bessctl
+apt-get install -y python-scapy libgraph-easy-perl docker.io
