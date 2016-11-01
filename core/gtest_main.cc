1d4cab98ac23c14262fbf70590740d63e3babda8
Sangjin Han <sangjin@eecs.berkeley.edu>
2016-11-01 01:02:52 -0700

    Initialize glog before running test cases

--- core/gtest_main.cc
+++ core/gtest_main.cc
@@ -1,6 +1,8 @@
+#include <glog/logging.h>
 #include <gtest/gtest.h>
 
 int main(int argc, char **argv) {
+  google::InitGoogleLogging(argv[0]);
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
