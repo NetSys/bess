513465c0c2848eae9c7d8f265643dba4e4a0c483
Sangjin Han <sangjin@eecs.berkeley.edu>
2016-11-06 20:23:13 -0800

    Simplify FormatVarg() with vasprintf

--- core/utils/format.cc
+++ core/utils/format.cc
@@ -15,19 +15,14 @@ std::string Format(const char *fmt, ...) {
 }
 
 std::string FormatVarg(const char *fmt, va_list ap) {
-  const int init_bufsize = 128;
-  std::unique_ptr<char[]> buf;
+  char *ptr = nullptr;
+  int len = vasprintf(&ptr, fmt, ap);
+  if (len < 0)
+    return "<FormatVarg() error>";
 
-  buf.reset(new char[init_bufsize]);
-  int ret = vsnprintf(&buf[0], init_bufsize, fmt, ap);
-
-  if (ret >= init_bufsize) {
-    buf.reset(new char[ret + 1]);
-    int new_ret;
-    new_ret = vsnprintf(&buf[0], ret + 1, fmt, ap);
-    assert(new_ret == ret);
-  }
-  return std::string(buf.get());
+  std::string ret(ptr, len);
+  free(ptr);
+  return ret;
 }
 
 }  // namespace utils
