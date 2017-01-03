278e784037d4ea15efe54ec2eeb4bd0f83c9a327
Chang Lan <clan@eecs.berkeley.edu>
2017-01-03 13:54:58 -0800

    nat: Add NAT module implementation

---
+++ core/modules/nat.cc
@@ -0,0 +1,134 @@
+#include "nat.h"
+
+#include <rte_ether.h>
+#include <rte_ip.h>
+#include <rte_udp.h>
+
+#include <numeric>
+#include <string>
+
+const uint16_t MIN_PORT = 1024;
+const uint16_t MAX_PORT = 65535;
+const uint64_t TIME_OUT = 12e10;
+
+const Commands NAT::cmds = {
+    {"add", "NATArg", MODULE_CMD_FUNC(&NAT::CommandAdd), 0},
+    {"clear", "EmptyArg", MODULE_CMD_FUNC(&NAT::CommandClear), 0}};
+
+pb_error_t NAT::Init(const bess::pb::NATArg &arg) {
+  for (const auto &rule : arg.rules()) {
+    CIDRNetwork int_net(rule.internal_addr_block());
+    CIDRNetwork ext_net(rule.external_addr_block());
+    rules_.push_back(std::make_pair(int_net, ext_net));
+  }
+
+  available_ports_.reserve(MAX_PORT - MIN_PORT + 1);
+  std::iota(available_ports_.begin(), available_ports_.end(), MIN_PORT);
+
+  flow_vec_.reserve(MAX_PORT - MIN_PORT + 1);
+
+  return pb_errno(0);
+}
+
+pb_cmd_response_t NAT::CommandAdd(const bess::pb::NATArg &arg) {
+  for (const auto &rule : arg.rules()) {
+    CIDRNetwork int_net(rule.internal_addr_block());
+    CIDRNetwork ext_net(rule.external_addr_block());
+    rules_.push_back(std::make_pair(int_net, ext_net));
+  }
+  return pb_cmd_response_t();
+}
+
+pb_cmd_response_t NAT::CommandClear(const bess::pb::EmptyArg &) {
+  rules_.clear();
+  flow_hash_.clear();
+  flow_vec_.clear();
+  available_ports_.reserve(MAX_PORT - MIN_PORT + 1);
+  std::iota(available_ports_.begin(), available_ports_.end(), MIN_PORT);
+  return pb_cmd_response_t();
+}
+
+void NAT::ProcessBatch(bess::PacketBatch *batch) {
+  gate_idx_t out_gates[bess::PacketBatch::kMaxBurst];
+  gate_idx_t incoming_gate = get_igate();
+
+  int cnt = batch->cnt();
+
+  for (int i = 0; i < cnt; i++) {
+    bess::Packet *pkt = batch->pkts()[i];
+
+    // By default, packets from igate k are sent to ogate k
+    out_gates[i] = incoming_gate;
+
+    struct ether_hdr *eth = pkt->head_data<struct ether_hdr *>();
+    struct ipv4_hdr *ip = reinterpret_cast<struct ipv4_hdr *>(eth + 1);
+    int ip_bytes = (ip->version_ihl & 0xf) << 2;
+    struct udp_hdr *udp = reinterpret_cast<struct udp_hdr *>(
+        reinterpret_cast<uint8_t *>(ip) + ip_bytes);
+
+    NAT::Flow flow = {.src_ip = ip->src_addr,
+                      .src_port = udp->src_port,
+                      .dst_ip = ip->dst_addr,
+                      .dst_port = udp->dst_port};
+
+    uint64_t now = tsc_to_ns(rdtsc());
+    auto hash_it = flow_hash_.find(flow);
+
+    if (hash_it != flow_hash_.end() && now - hash_it->second.time < TIME_OUT) {
+      // Entry exists and does not exceed timeout
+      const NAT::Flow &rev_flow = (incoming_gate == 0)
+                                      ? hash_it->second.reverse_flow
+                                      : hash_it->second.forward_flow;
+      hash_it->second.time = now;
+      ip->src_addr = rev_flow.src_ip;
+      ip->dst_addr = rev_flow.dst_ip;
+      udp->src_port = rev_flow.src_port;
+      udp->dst_port = rev_flow.dst_port;
+      ip->hdr_checksum = rte_ipv4_cksum(ip);
+    } else {
+      // Reclaim expired record
+      if (hash_it != flow_hash_.end() &&
+          now - hash_it->second.time >= TIME_OUT) {
+        available_ports_.push_back(hash_it->second.port);
+        flow_hash_.erase(hash_it);
+      }
+      if (incoming_gate == 1) {
+        out_gates[i] = DROP_GATE;  // Drop reverse flow
+        continue;
+      }
+
+      const auto rule_it = std::find_if(rules_.begin(), rules_.end(),
+                                        [&ip](const NATRule &rule) {
+                                          return rule.first.Match(ip->src_addr);
+                                        });
+      if (rule_it == rules_.end()) {
+        // No rules found for this source IP address, drop.
+        out_gates[i] = DROP_GATE;
+        continue;
+      }
+
+      uint16_t new_port = available_ports_.back();
+      available_ports_.pop_back();
+      FlowRecord &record = flow_vec_[new_port - MIN_PORT];
+
+      NAT::Flow ext_flow = flow;
+      ext_flow.src_ip = RandomIP(rule_it->second);
+      ext_flow.src_port = new_port;
+
+      record.port = new_port;
+      record.time = now;
+      record.forward_flow = flow;
+      record.reverse_flow = ext_flow.reverse_flow();
+
+      flow_hash_.insert({record.forward_flow, record});
+      flow_hash_.insert({record.reverse_flow, record});
+
+      ip->src_addr = ext_flow.src_ip;
+      ip->dst_addr = ext_flow.dst_ip;
+      udp->src_port = ext_flow.src_port;
+      udp->dst_port = ext_flow.dst_port;
+      ip->hdr_checksum = rte_ipv4_cksum(ip);
+    }
+  }
+  RunSplit(out_gates, batch);
+}
---
+++ core/modules/nat.h
@@ -0,0 +1,89 @@
+#ifndef BESS_MODULES_NAT_H_
+#define BESS_MODULES_NAT_H_
+
+#include <arpa/inet.h>
+
+#include <map>
+#include <utility>
+#include <vector>
+
+#include "../module.h"
+#include "../module_msg.pb.h"
+#include "../utils/ip.h"
+#include "../utils/random.h"
+
+using bess::utils::IPAddress;
+using bess::utils::CIDRNetwork;
+
+typedef std::pair<CIDRNetwork, CIDRNetwork> NATRule;
+
+template <class T>
+inline void hash_combine(std::size_t &seed, const T &v) {
+  std::hash<T> hasher;
+  seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+class NAT final : public Module {
+ public:
+  struct Flow {
+    IPAddress src_ip;
+    uint16_t src_port;
+    IPAddress dst_ip;
+    uint16_t dst_port;
+
+    Flow reverse_flow() {
+      return {.src_ip = dst_ip,
+              .src_port = dst_port,
+              .dst_ip = src_ip,
+              .dst_port = src_port};
+    }
+
+    bool operator==(const Flow &other) const {
+      return src_ip == other.src_ip && src_port == other.src_port &&
+             dst_ip == other.dst_ip && dst_port == other.dst_port;
+    }
+  };
+
+  struct FlowHash {
+    std::size_t operator()(const Flow &f) const {
+      std::size_t seed = 0;
+      hash_combine(seed, f.src_ip);
+      hash_combine(seed, f.src_port);
+      hash_combine(seed, f.dst_ip);
+      hash_combine(seed, f.dst_port);
+      return seed;
+    }
+  };
+
+  struct FlowRecord {
+    uint16_t port;
+    Flow forward_flow;
+    Flow reverse_flow;
+    uint64_t time;
+    FlowRecord() : port(0), time(0) {}
+  };
+
+  static const Commands cmds;
+
+  pb_error_t Init(const bess::pb::NATArg &arg);
+
+  void ProcessBatch(bess::PacketBatch *batch) override;
+
+  pb_cmd_response_t CommandAdd(const bess::pb::NATArg &arg);
+  pb_cmd_response_t CommandClear(const bess::pb::EmptyArg &arg);
+
+ private:
+  IPAddress RandomIP(const CIDRNetwork &net) {
+    uint32_t min = ntohl(net.addr & net.mask);
+    uint32_t max = ntohl(net.addr | (~net.mask));
+    return htonl(rng_.GetRange(max - min) + min);
+  }
+
+  std::vector<NATRule> rules_;
+  std::vector<uint16_t> available_ports_;
+  std::unordered_map<Flow, FlowRecord &, FlowHash> flow_hash_;
+  std::vector<FlowRecord> flow_vec_;
+  Random rng_;
+};
+
+#endif  // BESS_MODULES_NAT_H_
