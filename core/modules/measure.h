20e048c118f08b0fa87e64da6f485d835444308e
Melvin Walls <mwalls67@gmail.com>
2017-03-09 22:55:35 -0800

    modules/measure: add jitter measurements (#331)

--- core/modules/measure.h
+++ core/modules/measure.h
@@ -4,12 +4,17 @@
 #include "../module.h"
 #include "../module_msg.pb.h"
 #include "../utils/histogram.h"
+#include "../utils/random.h"
 
 class Measure final : public Module {
  public:
   Measure()
       : Module(),
-        hist_(Histogram<uint64_t>(kBuckets, kBucketWidth)),
+        rtt_hist_(Histogram<uint64_t>(kBuckets, kBucketWidth)),
+        jitter_hist_(Histogram<uint64_t>(kBuckets, kBucketWidth)),
+        rand_(Random()),
+        jitter_sample_prob_(),
+        last_rtt_ns_(),
         start_ns_(),
         warmup_ns_(),
         offset_(),
@@ -28,8 +33,15 @@ class Measure final : public Module {
  private:
   static const uint64_t kBucketWidth = 100;  // Measure in 100 ns units
   static const uint64_t kBuckets = 1000000;
+  static constexpr double kDefaultIpDvSampleProb = 0.05;
 
-  Histogram<uint64_t> hist_;
+  Histogram<uint64_t> rtt_hist_;
+  Histogram<uint64_t> jitter_hist_;
+
+  Random rand_;
+  double jitter_sample_prob_;
+
+  uint64_t last_rtt_ns_;
 
   uint64_t start_ns_;
   uint64_t warmup_ns_;  // no measurement for this warmup period
