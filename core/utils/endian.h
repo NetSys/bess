56517617ed3c65ae0d67bffb9a428f5546931f71
Chang Lan <clan@eecs.berkeley.edu>
2017-03-13 17:48:55 -0700

    Add equality checks to BigEndian types

--- core/utils/endian.h
+++ core/utils/endian.h
@@ -54,6 +54,10 @@ class[[gnu::packed]] BigEndian final : public EndianBase<T> {
     return is_be_system() ? v : EndianBase<T>::swap(v);
   }
 
+  bool operator==(const BigEndian &other) { return value_ == other.value_; }
+
+  bool operator!=(const BigEndian &other) { return value_ != other.value_; }
+
  protected:
   T value_;
 };
