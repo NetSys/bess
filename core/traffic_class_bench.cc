5310099c7af1a360ddc500d7a13dc899136c6084
Daniele Di Proietto <ddp@nefeli.io>
2017-03-27 18:02:31 -0700

    traffic_class_bench: Single Leaf, many Tasks.

--- core/traffic_class_bench.cc
+++ core/traffic_class_bench.cc
@@ -199,6 +199,85 @@ BENCHMARK_DEFINE_F(TCRoundRobin, TCScheduleOnce)(benchmark::State &state) {
 }
 
 BENCHMARK_REGISTER_F(TCRoundRobin, TCScheduleOnce)
+    ->Args({1 << 0})
+    ->Args({4 << 0})
+    ->Args({4 << 1})
+    ->Args({4 << 2})
+    ->Args({4 << 3})
+    ->Args({4 << 4})
+    ->Args({4 << 5})
+    ->Args({4 << 6})
+    ->Args({4 << 7})
+    ->Args({4 << 8})
+    ->Args({4 << 9})
+    ->Args({4 << 10})
+    ->Args({4 << 11})
+    ->Args({4 << 12})
+    ->Args({4 << 13})
+    ->Args({4 << 14})
+    ->Complexity();
+
+// Performs TC Scheduler init/deinit before/after each test.
+class TCLeaf : public benchmark::Fixture {
+ public:
+  TCLeaf() : s_(), dummy_(), tasks_() {}
+
+  void SetUp(benchmark::State &state) override {
+    int num_tasks = state.range(0);
+
+    dummy_ = new DummyModule;
+
+    TrafficClass *root = CT("rr", {ROUND_ROBIN}, {});
+    s_ = new Scheduler(root);
+    RoundRobinTrafficClass *rr =
+        static_cast<RoundRobinTrafficClass *>(TrafficClassBuilder::Find("rr"));
+    tasks_ = new Task *[num_tasks];
+
+    std::string name("class_leaf");
+    LeafTrafficClass *c =
+        TrafficClassBuilder::CreateTrafficClass<LeafTrafficClass>(name);
+    CHECK(rr->AddChild(c));
+    for (int i = 0; i < num_tasks; i++) {
+      tasks_[i] = new Task(dummy_, nullptr, c);
+
+    }
+    CHECK(!rr->blocked());
+  }
+
+  void TearDown(benchmark::State &state) override {
+    int num_classes = state.range(0);
+
+    delete s_;
+    s_ = nullptr;
+
+    delete dummy_;
+    dummy_ = nullptr;
+
+    for (int i = 0; i < num_classes; i++) {
+      delete tasks_[i];
+    }
+    delete[] tasks_;
+    tasks_ = nullptr;
+
+    TrafficClassBuilder::ClearAll();
+  }
+
+ protected:
+  Scheduler *s_;
+  Module *dummy_;
+  Task **tasks_;
+};
+
+BENCHMARK_DEFINE_F(TCLeaf, TCRunTaskOnce)(benchmark::State &state) {
+  while (state.KeepRunning()) {
+    s_->ScheduleOnce();
+  }
+  state.SetItemsProcessed(state.iterations() * state.range(0));
+  state.SetComplexityN(state.range(0));
+}
+
+BENCHMARK_REGISTER_F(TCLeaf, TCRunTaskOnce)
+    ->Args({1 << 0})
     ->Args({4 << 0})
     ->Args({4 << 1})
     ->Args({4 << 2})
