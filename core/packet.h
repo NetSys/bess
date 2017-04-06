NetSys/bess

commit ba8f0a650b4a320f5f7937634bdc7c914a58e1ed

pull rte_mbuf fields into Packet

core/packet.h
@@ -4,6 +4,7 @@
 #include <cassert>
 #include <type_traits>
 
+#include <rte_atomic.h>
 #include <rte_config.h>
 #include <rte_mbuf.h>
 
@@ -20,11 +21,6 @@ static_assert(SNBUF_HEADROOM == RTE_PKTMBUF_HEADROOM,
               "DPDK compatibility check failed");
 
 namespace bess {
-
-// should probably be a static member of Packet
-inline phys_addr_t seg_dma_addr(struct rte_mbuf *mbuf) {
-  return mbuf->buf_physaddr + mbuf->data_off;
-}
 
 class Packet;
 typedef Packet **PacketArray;
@@ -52,9 +48,12 @@ class Packet {
  public:
   // TODO: delete constructor/destructor w/o breaking module_bench
 
-  // TODO: get rid of me
-  struct rte_mbuf &mbuf() {
-    return mbuf_;
+  struct rte_mbuf &as_rte_mbuf() {
+    return *reinterpret_cast<struct rte_mbuf *>(this);
+  }
+
+  const struct rte_mbuf &as_rte_mbuf() const {
+    return *reinterpret_cast<const struct rte_mbuf *>(this);
   }
 
   Packet *vaddr() const { return vaddr_; }
@@ -75,8 +74,9 @@ class Packet {
   }
 
   template <typename T = void *>
-  T head_data() {
-    return rte_pktmbuf_mtod(&mbuf_, T);
+  T head_data(uint16_t offset = 0) {
+    return reinterpret_cast<T>(static_cast<char *>(buf_addr_) + data_off_ +
+                               offset);
   }
 
   template <typename T = char *>
@@ -96,65 +96,80 @@ class Packet {
 
   template <typename T = void *>
   T buffer() {
-    return reinterpret_cast<T>(mbuf_.buf_addr);
+    return reinterpret_cast<T>(buf_addr_);
   }
 
-  int nb_segs() const { return mbuf_.nb_segs; }
-  void set_nb_segs(int n) { mbuf_.nb_segs = n; }
+  int nb_segs() const { return nb_segs_; }
+  void set_nb_segs(int n) { nb_segs_ = n; }
 
-  Packet *next() const { return reinterpret_cast<Packet *>(mbuf_.next); }
-  void set_next(Packet *next) {
-    mbuf_.next = reinterpret_cast<struct rte_mbuf *>(next);
+  Packet *next() const { return next_; }
+  void set_next(Packet *next) { next_ = next; }
+
+  uint16_t data_off() { return data_off_; }
+  void set_data_off(uint16_t offset) { data_off_ = offset; }
+
+  uint16_t data_len() { return data_len_; }
+  void set_data_len(uint16_t len) { data_len_ = len; }
+
+  int head_len() const { return data_len_; }
+
+  int total_len() const { return pkt_len_; }
+  void set_total_len(uint32_t len) { pkt_len_ = len; }
+
+  uint16_t refcnt() const { return rte_mbuf_refcnt_read(&as_rte_mbuf()); }
+
+  void set_refcnt(uint16_t cnt) { rte_mbuf_refcnt_set(&as_rte_mbuf(), cnt); }
+
+  // add cnt to refcnt
+  void update_refcnt(uint16_t cnt) {
+    rte_mbuf_refcnt_update(&as_rte_mbuf(), cnt);
   }
 
-  uint16_t data_off() { return mbuf_.data_off; }
-  void set_data_off(uint16_t offset) { mbuf_.data_off = offset; }
+  uint16_t headroom() const { return rte_pktmbuf_headroom(&as_rte_mbuf()); }
 
-  uint16_t data_len() { return mbuf_.data_len; }
-  void set_data_len(uint16_t len) { mbuf_.data_len = len; }
-
-  int head_len() const { return rte_pktmbuf_data_len(&mbuf_); }
-
-  int total_len() const { return rte_pktmbuf_pkt_len(&mbuf_); }
-  void set_total_len(uint32_t len) { mbuf_.pkt_len = len; }
+  uint16_t tailroom() const { return rte_pktmbuf_tailroom(&as_rte_mbuf()); }
 
   // single segment?
-  int is_linear() const { return rte_pktmbuf_is_contiguous(&mbuf_); }
+  int is_linear() const { return rte_pktmbuf_is_contiguous(&as_rte_mbuf()); }
 
   // single segment and direct?
-  int is_simple() const { return is_linear() && RTE_MBUF_DIRECT(&mbuf_); }
+  int is_simple() const {
+    return is_linear() && RTE_MBUF_DIRECT(&as_rte_mbuf());
+  }
+
+  void reset() { rte_pktmbuf_reset(&as_rte_mbuf()); }
 
   void *prepend(uint16_t len) {
-    if (unlikely(mbuf_.data_off < len))
+    if (unlikely(data_off_ < len))
       return nullptr;
 
-    mbuf_.data_off -= len;
-    mbuf_.data_len += len;
-    mbuf_.pkt_len += len;
+    data_off_ -= len;
+    data_len_ += len;
+    pkt_len_ += len;
 
     return head_data();
   }
 
   // remove bytes from the beginning
   void *adj(uint16_t len) {
-    if (unlikely(mbuf_.data_len < len))
+    if (unlikely(data_len_ < len))
       return nullptr;
 
-    mbuf_.data_off += len;
-    mbuf_.data_len -= len;
-    mbuf_.pkt_len -= len;
+    data_off_ += len;
+    data_len_ -= len;
+    pkt_len_ -= len;
 
     return head_data();
   }
 
   // add bytes to the end
-  void *append(uint16_t len) { return rte_pktmbuf_append(&mbuf_, len); }
+  void *append(uint16_t len) { return rte_pktmbuf_append(&as_rte_mbuf(), len); }
 
   // remove bytes from the end
   void trim(uint16_t to_remove) {
     int ret;
 
-    ret = rte_pktmbuf_trim(&mbuf_, to_remove);
+    ret = rte_pktmbuf_trim(&as_rte_mbuf(), to_remove);
     assert(ret == 0);
   }
 
@@ -163,7 +178,7 @@ class Packet {
 
     assert(src->is_linear());
 
-    dst = __packet_alloc_pool(src->mbuf_.pool);
+    dst = __packet_alloc_pool(src->pool_);
 
     rte_memcpy(dst->append(src->total_len()), src->head_data(),
                src->total_len());
@@ -171,9 +186,7 @@ class Packet {
     return dst;
   }
 
-  phys_addr_t dma_addr() { return seg_dma_addr(&mbuf_); }
-
-  // TODO: stream operator
+  phys_addr_t dma_addr() { return buf_physaddr_ + data_off_; }
 
   std::string Dump();
 
@@ -197,9 +210,108 @@ class Packet {
   static void Free(PacketBatch *batch) { Free(batch->pkts(), batch->cnt()); }
 
  private:
+  typedef void *MARKER[0];     // generic marker for a point in a structure
+  typedef uint8_t MARKER8[0];  // generic marker with 1B alignment
+
   union {
-    struct rte_mbuf mbuf_;
-    char _mbuf_[SNBUF_MBUF];
+    // This is all lifted from rte_mbuf.h
+    struct {
+      MARKER cacheline0_;
+
+      void *buf_addr_;            // Virtual address of segment buffer.
+      phys_addr_t buf_physaddr_;  // Physical address of segment buffer.
+
+      uint16_t buf_len_;  // Length of segment buffer.
+
+      // next 6 bytes are initialised on RX descriptor rearm
+      MARKER8 rearm_data_;
+      uint16_t data_off_;
+
+      /**
+       * 16-bit Reference counter.
+       * It should only be accessed using the following functions:
+       * rte_mbuf_refcnt_update(), rte_mbuf_refcnt_read(), and
+       * rte_mbuf_refcnt_set(). The functionality of these functions (atomic,
+       * or non-atomic) is controlled by the CONFIG_RTE_MBUF_REFCNT_ATOMIC
+       * config option.
+       */
+      union {
+        rte_atomic16_t refcnt_atomic_;  // Atomically accessed refcnt
+        uint16_t refcnt_;               // Non-atomically accessed refcnt
+      };
+      uint8_t nb_segs_;  // Number of segments.
+      uint8_t port_;     // Input port.
+
+      uint64_t offload_flags_;  // Offload features.
+
+      // remaining bytes are set on RX when pulling packet from descriptor
+      MARKER rx_descriptor_fields1_;
+
+      /*
+       * The packet type, which is the combination of outer/inner L2, L3, L4
+       * and tunnel types. The packet_type is about data really present in the
+       * mbuf. Example: if vlan stripping is enabled, a received vlan packet
+       * would have RTE_PTYPE_L2_ETHER and not RTE_PTYPE_L2_VLAN because the
+       * vlan is stripped from the data.
+       */
+      union {
+        uint32_t packet_type_;  // L2/L3/L4 and tunnel information.
+        struct {
+          uint32_t l2_type_ : 4;        // (Outer) L2 type.
+          uint32_t l3_type_ : 4;        // (Outer) L3 type.
+          uint32_t l4_type_ : 4;        // (Outer) L4 type.
+          uint32_t tun_type_ : 4;       // Tunnel type.
+          uint32_t inner_l2_type_ : 4;  // Inner L2 type.
+          uint32_t inner_l3_type_ : 4;  // Inner L3 type.
+          uint32_t inner_l4_type_ : 4;  // Inner L4 type.
+        };
+      };
+
+      uint32_t pkt_len_;   // Total pkt len: sum of all segments.
+      uint16_t data_len_;  // Amount of data in segment buffer.
+
+      // VLAN TCI (CPU order), valid if PKT_RX_VLAN_STRIPPED is set.
+      uint16_t vlan_tci_;
+
+      union {
+        uint32_t rss_;  // RSS hash result if RSS enabled
+        struct {
+          union {
+            struct {
+              uint16_t hash_;
+              uint16_t id_;
+            };
+            uint32_t lo_;
+            // Second 4 flexible bytes
+          };
+          uint32_t hi_;
+          // First 4 flexible bytes or FD ID, dependent on
+          // PKT_RX_FDIR_* flag in ol_flags.
+        } fdir_;  // Filter identifier if FDIR enabled
+        struct {
+          uint32_t lo_;
+          uint32_t hi_;
+        } sched_;       // Hierarchical scheduler
+        uint32_t usr_;  // User defined tags. See rte_distributor_process()
+      } hash_;          // hash information
+
+      uint32_t seqn_;  // Sequence number.
+
+      // Outer VLAN TCI (CPU order), valid if PKT_RX_QINQ_STRIPPED is set.
+      uint16_t vlan_tci_outer_;
+
+      // second cache line - fields only used in slow path or on TX
+      MARKER cacheline1_ __rte_cache_min_aligned;
+
+      union {
+        void *userdata_;    // Can be used for external metadata
+        uint64_t udata64_;  // Allow 8-byte userdata on 32-bit
+      };
+
+      struct rte_mempool *pool_;  // Pool from which mbuf was allocated.
+      Packet *next_;              // Next segment of scattered packet.
+    };
+    char mbuf_[SNBUF_MBUF];
   };
 
   union {
@@ -238,7 +350,7 @@ class Packet {
 
 static_assert(std::is_pod<Packet>::value, "Packet is not a POD Type");
 
-extern struct rte_mbuf pframe_template;
+extern Packet pframe_template;
 
 #if __AVX__
 #include "packet_avx.h"
@@ -254,25 +366,25 @@ int Packet::Alloc(PacketArray pkts, size_t cnt, uint16_t len) {
   for (i = 0; i < cnt; i++) {
     Packet *pkt = pkts[i];
 
-    rte_mbuf_refcnt_set(&pkt->mbuf_, 1);
-    rte_pktmbuf_reset(&pkt->mbuf_);
+    pkt->set_refcnt(1);
+    pkt->reset();
 
-    pkt->mbuf.pkt_len = pkt->mbuf_.data_len = len;
+    pkt->pkt_len_ = pkt->data_len_ = len;
   }
 
   return cnt;
 }
 
 void Packet::Free(PacketArray pkts, int cnt) {
-  struct rte_mempool *pool = pkts[0]->mbuf_.pool;
+  struct rte_mempool *pool = pkts[0]->pool_;
 
   int i;
 
   for (i = 0; i < cnt; i++) {
-    struct rte_mbuf *mbuf = &pkts[i]->mbuf_;
+    Packet *pkt = &pkts[i];
 
-    if (unlikely(mbuf->pool != pool || !snb_is_simple(pkts[i]) ||
-                 rte_mbuf_refcnt_read(mbuf) != 1)) {
+    if (unlikely(pkt->pool_ != pool || pkt->is_simple() ||
+                 pkt->refcnt() != 1)) {
       goto slow_path;
     }
   }

core/module_bench.cc
@@ -21,7 +21,7 @@ class DummySourceModule : public Module {
     bess::Packet *pkt = &pkts[i];
 
     // this fake packet must not be freed
-    rte_mbuf_refcnt_set(reinterpret_cast<struct rte_mbuf *>(pkt), 2);
+    pkt->set_refcnt(2);
 
     // not chained
     pkt->set_next(nullptr);

core/packet.cc
@@ -14,7 +14,7 @@
 
 namespace bess {
 
-struct rte_mbuf pframe_template;
+Packet pframe_template;
 
 static struct rte_mempool *pframe_pool[RTE_MAX_NUMA_NODES];
 
@@ -74,14 +74,14 @@ static void init_templates(void) {
   int i;
 
   for (i = 0; i < RTE_MAX_NUMA_NODES; i++) {
-    struct rte_mbuf *mbuf;
+    Packet *pkt;
 
     if (!pframe_pool[i])
       continue;
 
-    mbuf = rte_pktmbuf_alloc(pframe_pool[i]);
-    pframe_template = *mbuf;
-    rte_pktmbuf_free(mbuf);
+    pkt = reinterpret_cast<Packet *>(rte_pktmbuf_alloc(pframe_pool[i]));
+    pframe_template = *pkt;
+    Packet::Free(pkt);
   }
 }
 

core/packet_avx.h
@@ -26,7 +26,7 @@ int Packet::Alloc(PacketArray pkts, size_t cnt, uint16_t len) {
   if (ret != 0)
     return 0;
 
-  mbuf_template = *((__m128i *)&pframe_template.buf_len);
+  mbuf_template = *((__m128i *)&pframe_template.buf_len_);
 
   /* 4 at a time didn't help */
   for (i = 0; i < (cnt & (~0x1)); i += 2) {
@@ -35,18 +35,18 @@ int Packet::Alloc(PacketArray pkts, size_t cnt, uint16_t len) {
     Packet *pkt0 = pkts[i];
     Packet *pkt1 = pkts[i + 1];
 
-    _mm_storeu_si128((__m128i *)&pkt0->mbuf_.buf_len, mbuf_template);
-    _mm_storeu_si128((__m128i *)&pkt0->mbuf_.packet_type, rxdesc_fields);
+    _mm_storeu_si128((__m128i *)&pkt0->buf_len_, mbuf_template);
+    _mm_storeu_si128((__m128i *)&pkt0->packet_type_, rxdesc_fields);
 
-    _mm_storeu_si128((__m128i *)&pkt1->mbuf_.buf_len, mbuf_template);
-    _mm_storeu_si128((__m128i *)&pkt1->mbuf_.packet_type, rxdesc_fields);
+    _mm_storeu_si128((__m128i *)&pkt1->buf_len_, mbuf_template);
+    _mm_storeu_si128((__m128i *)&pkt1->packet_type_, rxdesc_fields);
   }
 
   if (cnt & 0x1) {
     Packet *pkt = pkts[i];
 
-    _mm_storeu_si128((__m128i *)&pkt->mbuf_.buf_len, mbuf_template);
-    _mm_storeu_si128((__m128i *)&pkt->mbuf_.packet_type, rxdesc_fields);
+    _mm_storeu_si128((__m128i *)&pkt->buf_len_, mbuf_template);
+    _mm_storeu_si128((__m128i *)&pkt->packet_type_, rxdesc_fields);
   }
 
   return cnt;
@@ -60,7 +60,7 @@ int Packet::Alloc(PacketArray pkts, size_t cnt, uint16_t len) {
  *    (Do not use RTE_MBUF_(IN)DIRECT, since there is a difference
  *     between DPDK 1.8 and 2.0) */
 void Packet::Free(PacketArray pkts, int cnt) {
-  struct rte_mempool *_pool = pkts[0]->mbuf_.pool;
+  struct rte_mempool *_pool = pkts[0]->pool_;
 
   /* broadcast */
   __m128i offset = _mm_set1_epi64x(SNBUF_HEADROOM_OFF);
@@ -105,9 +105,9 @@ void Packet::Free(PacketArray pkts, int cnt) {
   if (i < cnt) {
     Packet *pkt = pkts[i];
 
-    if (unlikely(pkt->mbuf_.pool != _pool || pkt->mbuf_.next != nullptr ||
-                 rte_mbuf_refcnt_read(&pkt->mbuf_) != 1 ||
-                 pkt->mbuf_.buf_addr != pkt->headroom_)) {
+    if (unlikely(pkt->pool_ != _pool || pkt->next_ != nullptr ||
+                 rte_mbuf_refcnt_read(&pkt->as_rte_mbuf()) != 1 ||
+                 pkt->buf_addr_ != pkt->headroom_)) {
       goto slow_path;
     }
   }

commit 03c1830c16446b4681bec95dde63ee3fcd13205a

Fix batched Free() on non-AVX machines
Regression from ba8f0a65

core/packet.h
@@ -402,7 +402,7 @@ inline void Packet::Free(Packet **pkts, size_t cnt) {
   for (size_t i = 0; i < cnt; i++) {
     const Packet *pkt = pkts[i];
 
-    if (unlikely(pkt->pool_ != pool || pkt->is_simple() ||
+    if (unlikely(pkt->pool_ != pool || !pkt->is_simple() ||
                  pkt->refcnt() != 1)) {
       goto slow_path;
     }