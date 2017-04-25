470f5a2a7aa31946d8138fa338ef1f3aaad4f752
James Murphy <jmurphy@arista.com>
2017-04-25 12:48:00 -0700

    Add a modules option to load the modules from an arbitrary directory (#415)
    Add a modules option to allow bessd to load the modules from an arbitrary
    directory.

--- core/opts.cc
+++ core/opts.cc
@@ -5,6 +5,7 @@
 #include <cstdint>
 
 #include "worker.h"
+#include "bessd.h"
 
 // Port this BESS instance listens on.
 // Panda came up with this default number
@@ -19,6 +20,8 @@ DEFINE_bool(s, false, "Show TC statistics every second");
 DEFINE_bool(d, false, "Run BESS in debug mode (with debug log messages)");
 DEFINE_bool(a, false, "Allow multiple instances");
 DEFINE_bool(no_huge, false, "Disable hugepages");
+DEFINE_string(modules, bess::bessd::GetCurrentDirectory() + "modules",
+	      "Load modules from the specified directory");
 
 static bool ValidateCoreID(const char *, int32_t value) {
   if (!is_cpu_present(value)) {
