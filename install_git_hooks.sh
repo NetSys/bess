75749c66576367fc16a5606a578edb15c77e37e6
Chang Lan <clan@eecs.berkeley.edu>
2017-04-28 13:45:09 -0700

    git: Fix a number of issues in hooks
    - Fix pre-commit hook
    - Make sure pwd is the root directory of BESS repo
    - Make sure clang-format and autopep8 exist before installing hooks

--- install_git_hooks.sh
+++ install_git_hooks.sh
@@ -1,2 +1,34 @@
 #!/bin/bash
-for i in .hooks/*; do ln -s "../../${i}" ".git/hooks/$(basename ${i})"; done
+
+CXX_FORMAT="clang-format"
+PY_FORMAT="autopep8"
+
+check_directory() {
+  if [ ! -d $1 ]; then
+    echo "Directory $1 not found."
+    echo "Please make sure you are running this script in the root directory of BESS repo."
+    exit 1
+  fi
+}
+
+command_exists() {
+  command -v $1 >/dev/null 2>&1
+}
+
+check_directory .git
+check_directory .hooks
+
+if ! command_exists $CXX_FORMAT; then
+  echo "Error: $CXX_FORMAT executable not found.\n" >&2
+  exit 1
+fi
+
+if ! command_exists $PY_FORMAT; then
+  echo "Error: $PY_FORMAT executable not found. Try 'pip install $PY_FORMAT'?\n" >&2
+  exit 1
+fi
+
+for i in .hooks/*; do
+  rm -rf .git/hooks/$(basename ${i})
+  ln -s ../../${i} .git/hooks/$(basename ${i})
+done
