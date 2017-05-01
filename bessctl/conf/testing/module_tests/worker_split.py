7d135bb563058f42d22a71744adfb8fd9090243b
Sangjin Han <sangjin@eecs.berkeley.edu>
2017-04-30 22:15:11 -0700

    Add WorkerSplit module (#405)
    * Add WorkerSplit module
    
    This module simply forwards a packet batch to output gate X, where X
    is the id of worker thread that is running the module at the moment.
    
    * Add a sample for WorkerSplit
    
    * Add module test for WorkerSplit

---
+++ bessctl/conf/testing/module_tests/worker_split.py
@@ -0,0 +1,34 @@
+import time
+
+
+def test_workersplit():
+    NUM_WORKERS = 2
+
+    for i in range(NUM_WORKERS):
+        bess.add_worker(wid=i, core=i)
+
+    for wid in range(NUM_WORKERS):
+        src = Source()
+        ws = WorkerSplit()
+        src -> ws
+
+        for i in range(NUM_WORKERS):
+            ws:i -> Sink()
+
+        bess.attach_module(src.name, wid=wid)
+
+        bess.resume_all()
+        time.sleep(0.1)
+        bess.pause_all()
+
+        # packets should flow onto only one output gate...
+        ogates = bess.get_module_info(ws.name).ogates
+        for ogate in ogates:
+            if ogate.ogate == wid:
+                assert ogate.pkts > 0
+            else:
+                assert ogate.pkts == 0
+
+        bess.reset_modules()
+
+CUSTOM_TEST_FUNCTIONS.append(test_workersplit)
