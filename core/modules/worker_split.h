db5c55b088103909be8d8bd20df632154a521747
Aurojit Panda <aurojit@gmail.com>
2017-05-09 20:24:39 -0700

    Fix `WorkerSplit` and `Bypass` model (#446)
    * Mark `Bypass` and `WorkerSplit` thread safe
    
    Neither module has any state, and is currently used by multiple workers
    in the `workersplit` script.
    
    * Add worker_split to sanity_check.sh

--- core/modules/worker_split.h
+++ core/modules/worker_split.h
@@ -7,6 +7,8 @@ class WorkerSplit final : public Module {
  public:
   static const gate_idx_t kNumOGates = Worker::kMaxWorkers;
 
+  WorkerSplit() { max_allowed_workers_ = kNumOGates; }
+
   void ProcessBatch(bess::PacketBatch *batch) override;
 };
 
