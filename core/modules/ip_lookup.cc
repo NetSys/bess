f5b9b77d1b9ca9a0762c4df86ae30752a7b85362
Sangjin Han <sangjin@eecs.berkeley.edu>
2017-05-31 01:17:38 +0000

    Fix build errors with g++ -O0

--- core/modules/ip_lookup.cc
+++ core/modules/ip_lookup.cc
@@ -9,6 +9,109 @@
 
 #define VECTOR_OPTIMIZATION 1
 
+// Strangely, rte_lpm_lookupx4() in rte_lpm_sse.h causes a build error
+// when g++ is run with -O0 option.
+// error: the last argument must be an 8-bit immediate
+//   i24 = _mm_srli_si128(i24, sizeof(uint64_t));
+// ... although the last argument is indeed a compile-time constant.
+// To work around the issue as a quick and dirty fix, this function was copied
+// from DPDK 17.02 to modify its body. See #if 0 .. #endif block below.
+#ifndef __OPTIMIZE__
+static void lpm_lookupx4(const struct rte_lpm *lpm, __m128i ip, uint32_t hop[4],
+                         uint32_t defv) {
+  __m128i i24;
+  rte_xmm_t i8;
+  uint32_t tbl[4];
+  uint64_t idx, pt, pt2;
+  const uint32_t *ptbl;
+
+  const __m128i mask8 =
+      _mm_set_epi32(UINT8_MAX, UINT8_MAX, UINT8_MAX, UINT8_MAX);
+
+  /*
+   * RTE_LPM_VALID_EXT_ENTRY_BITMASK for 2 LPM entries
+   * as one 64-bit value (0x0300000003000000).
+   */
+  const uint64_t mask_xv = ((uint64_t)RTE_LPM_VALID_EXT_ENTRY_BITMASK |
+                            (uint64_t)RTE_LPM_VALID_EXT_ENTRY_BITMASK << 32);
+
+  /*
+   * RTE_LPM_LOOKUP_SUCCESS for 2 LPM entries
+   * as one 64-bit value (0x0100000001000000).
+   */
+  const uint64_t mask_v = ((uint64_t)RTE_LPM_LOOKUP_SUCCESS |
+                           (uint64_t)RTE_LPM_LOOKUP_SUCCESS << 32);
+
+  /* get 4 indexes for tbl24[]. */
+  i24 = _mm_srli_epi32(ip, CHAR_BIT);
+
+  /* extract values from tbl24[] */
+  idx = _mm_cvtsi128_si64(i24);
+#if 0
+	i24 = _mm_srli_si128(i24, sizeof(uint64_t));
+#else
+  i24 = _mm_srli_si128(i24, 8);
+#endif
+  ptbl = (const uint32_t *)&lpm->tbl24[(uint32_t)idx];
+  tbl[0] = *ptbl;
+  ptbl = (const uint32_t *)&lpm->tbl24[idx >> 32];
+  tbl[1] = *ptbl;
+
+  idx = _mm_cvtsi128_si64(i24);
+
+  ptbl = (const uint32_t *)&lpm->tbl24[(uint32_t)idx];
+  tbl[2] = *ptbl;
+  ptbl = (const uint32_t *)&lpm->tbl24[idx >> 32];
+  tbl[3] = *ptbl;
+
+  /* get 4 indexes for tbl8[]. */
+  i8.x = _mm_and_si128(ip, mask8);
+
+  pt = (uint64_t)tbl[0] | (uint64_t)tbl[1] << 32;
+  pt2 = (uint64_t)tbl[2] | (uint64_t)tbl[3] << 32;
+
+  /* search successfully finished for all 4 IP addresses. */
+  if (likely((pt & mask_xv) == mask_v) && likely((pt2 & mask_xv) == mask_v)) {
+    *(uint64_t *)hop = pt & RTE_LPM_MASKX4_RES;
+    *(uint64_t *)(hop + 2) = pt2 & RTE_LPM_MASKX4_RES;
+    return;
+  }
+
+  if (unlikely((pt & RTE_LPM_VALID_EXT_ENTRY_BITMASK) ==
+               RTE_LPM_VALID_EXT_ENTRY_BITMASK)) {
+    i8.u32[0] = i8.u32[0] + (uint8_t)tbl[0] * RTE_LPM_TBL8_GROUP_NUM_ENTRIES;
+    ptbl = (const uint32_t *)&lpm->tbl8[i8.u32[0]];
+    tbl[0] = *ptbl;
+  }
+  if (unlikely((pt >> 32 & RTE_LPM_VALID_EXT_ENTRY_BITMASK) ==
+               RTE_LPM_VALID_EXT_ENTRY_BITMASK)) {
+    i8.u32[1] = i8.u32[1] + (uint8_t)tbl[1] * RTE_LPM_TBL8_GROUP_NUM_ENTRIES;
+    ptbl = (const uint32_t *)&lpm->tbl8[i8.u32[1]];
+    tbl[1] = *ptbl;
+  }
+  if (unlikely((pt2 & RTE_LPM_VALID_EXT_ENTRY_BITMASK) ==
+               RTE_LPM_VALID_EXT_ENTRY_BITMASK)) {
+    i8.u32[2] = i8.u32[2] + (uint8_t)tbl[2] * RTE_LPM_TBL8_GROUP_NUM_ENTRIES;
+    ptbl = (const uint32_t *)&lpm->tbl8[i8.u32[2]];
+    tbl[2] = *ptbl;
+  }
+  if (unlikely((pt2 >> 32 & RTE_LPM_VALID_EXT_ENTRY_BITMASK) ==
+               RTE_LPM_VALID_EXT_ENTRY_BITMASK)) {
+    i8.u32[3] = i8.u32[3] + (uint8_t)tbl[3] * RTE_LPM_TBL8_GROUP_NUM_ENTRIES;
+    ptbl = (const uint32_t *)&lpm->tbl8[i8.u32[3]];
+    tbl[3] = *ptbl;
+  }
+
+  hop[0] = (tbl[0] & RTE_LPM_LOOKUP_SUCCESS) ? tbl[0] & 0x00FFFFFF : defv;
+  hop[1] = (tbl[1] & RTE_LPM_LOOKUP_SUCCESS) ? tbl[1] & 0x00FFFFFF : defv;
+  hop[2] = (tbl[2] & RTE_LPM_LOOKUP_SUCCESS) ? tbl[2] & 0x00FFFFFF : defv;
+  hop[3] = (tbl[3] & RTE_LPM_LOOKUP_SUCCESS) ? tbl[3] & 0x00FFFFFF : defv;
+}
+
+// Substitute DPDK's function with ours.
+#define rte_lpm_lookupx4 lpm_lookupx4
+#endif
+
 static inline int is_valid_gate(gate_idx_t gate) {
   return (gate < MAX_GATES || gate == DROP_GATE);
 }
