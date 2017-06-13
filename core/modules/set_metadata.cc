c73104dd569b1c3528f42093863376ed5e5e63ef
Sangjin Han <sangjin@eecs.berkeley.edu>
2017-06-13 04:21:38 +0000

    Store metadata attribute values in big endian

--- core/modules/set_metadata.cc
+++ core/modules/set_metadata.cc
@@ -62,8 +62,7 @@ CommandResponse SetMetadata::AddAttrOne(
   // stored in memory). value_bin is a short stream of bytes, which means that
   // its data will never be reordered.
   if (attr.value_case() == bess::pb::SetMetadataArg_Attribute::kValueInt) {
-    if (!bess::utils::uint64_to_bin(&value, attr.value_int(), size,
-                                    bess::utils::is_be_system())) {
+    if (!bess::utils::uint64_to_bin(&value, attr.value_int(), size, true)) {
       return CommandFailure(EINVAL,
                             "'value_int' field has not a "
                             "correct %zu-byte value",
