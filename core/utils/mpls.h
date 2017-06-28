52d37cdfa8d3b3875f09e3ec07cdbc7de2a7c68f
GalSagie <gal.sagie@gmail.com>
2017-06-28 10:03:32 +0300

    Add MPLS header

--- core/utils/mpls.h
+++ core/utils/mpls.h
@@ -6,19 +6,19 @@
 namespace bess {
 namespace utils {
 
-/* MPLS header definition, Reference: RFC 5462, RFC 3032
- *
- *  0                   1                   2                   3
- *  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
- * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
- * |                Label                  | TC  |S|       TTL     |
- * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
- *
- *	Label:  Label Value, 20 bits
- *	TC:     Traffic Class field, 3 bits
- *	S:      Bottom of Stack, 1 bit
- *	TTL:    Time to Live, 8 bits
- */
+// MPLS header definition, Reference: RFC 5462, RFC 3032
+//
+//  0                   1                   2                   3
+//  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// |                Label                  | TC  |S|       TTL     |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//
+//	Label:  Label Value, 20 bits
+//	TC:     Traffic Class field, 3 bits
+//	S:      Bottom of Stack, 1 bit
+//	TTL:    Time to Live, 8 bits
+
 struct[[gnu::packed]] Mpls {
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
   uint32_t ttl : 8;     // Time to Live, 8 bits
