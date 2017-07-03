49a0fc82c97a2d45ab3138779e947bdbfdcf9f44
Steven H. Wang <wang.steven.h@gmail.com>
2017-07-03 00:55:25 +0000

    vlan_split: add Dot1AD (QinQ) test cases

--- bessctl/conf/testing/module_tests/vlan.py
+++ bessctl/conf/testing/module_tests/vlan.py
@@ -4,15 +4,17 @@ import scapy.all as scapy
 CRASH_TEST_INPUTS.append([VLANSplit(), 1, 1])
 
 ## OUTPUT TESTS ##
-def output_test(vids, default_gate=0):
+def output_test(vids, double_tag=False, default_gate=0):
     def gen_packet(vid):
         eth = scapy.Ether(src='02:1e:67:9f:4d:ae', dst='06:16:3e:1b:72:32')
-        vlan = scapy.Dot1Q(vlan=vid)
+        vlan1 = scapy.Dot1AD(vlan=vid)
+        vlan2 = scapy.Dot1Q(vlan=vid)
         ip = scapy.IP()
         udp = scapy.UDP(sport=10001, dport=10002)
         payload = 'truculence'
-        pkt = eth / vlan / ip / udp / payload
-        return pkt
+        single_tag = eth / vlan2 / ip / udp / payload
+        double_tag = eth / vlan1 / vlan2 / ip / udp / payload
+        return single_tag, double_tag
 
     def gen_untagged_packet():
         eth = scapy.Ether(src='02:1e:67:9f:4d:ae', dst='06:16:3e:1b:72:32')
@@ -25,14 +27,21 @@ def output_test(vids, default_gate=0):
     expected = []
 
     for vid in vids:
-        p = gen_packet(vid)
+        p, pp = gen_packet(vid)
         q = gen_untagged_packet()
         if vid >= 0:
-            expected.append({
-                'input_port': 0,
-                'output_port': vid,
-                'input_packet': p,
-                'output_packet': q})
+            if not double_tag:
+                expected.append({
+                    'input_port': 0,
+                    'output_port': vid,
+                    'input_packet': p,
+                    'output_packet': q})
+            else:
+                expected.append({
+                    'input_port': 0,
+                    'output_port': vid,
+                    'input_packet': pp,
+                    'output_packet': p})
         else:
             expected.append({
                 'input_port': 0,
@@ -43,3 +52,4 @@ def output_test(vids, default_gate=0):
 
 OUTPUT_TEST_INPUTS.append(output_test([1, 100, 77, -1, 149, 50, 100, -1]))
 OUTPUT_TEST_INPUTS.append(output_test([100, 77, -1, 149, 50, 100, -1, 33, 70]))
+OUTPUT_TEST_INPUTS.append(output_test([100, 77, -1, 149, 50, 100, -1, 33, 70], True))
