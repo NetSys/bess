5891c0524fe85689ab23ba8d772a5e2eb36de503
Steven H. Wang <wang.steven.h@gmail.com>
2017-07-06 12:20:58 -0700

    Add VLANSplit module test (#546)
    * Add VLANSplit module test
    
    * Use pip to install scapy with Dot1AD support
    
    The apt-get python-scapy package for Xenial installs a version that
    doesn't have the Dot1AD layer (VLAN QinQ).
    
    * vlan_split: add Dot1AD (QinQ) test cases
    
    * improve wording
    
    * Disable vlan test for python 3
    
    The dev version of scapy, which has Dot1AD, is incompatible with
    Python3.

---
+++ bessctl/conf/testing/module_tests/vlan.py
@@ -0,0 +1,59 @@
+import scapy.all as scapy
+import sys
+
+if sys.version_info[0] != 2:
+    print("python3-compatible scapy does not have Dot1AD. Skipping vlan test...")
+else:
+    ## CRASH TESTS ##
+    CRASH_TEST_INPUTS.append([VLANSplit(), 1, 1])
+
+    ## OUTPUT TESTS ##
+    def output_test(vids, double_tag=False, default_gate=0):
+        def gen_packet(vid):
+            eth = scapy.Ether(src='02:1e:67:9f:4d:ae', dst='06:16:3e:1b:72:32')
+            vlan1 = scapy.Dot1AD(vlan=vid)
+            vlan2 = scapy.Dot1Q(vlan=vid)
+            ip = scapy.IP()
+            udp = scapy.UDP(sport=10001, dport=10002)
+            payload = 'truculence'
+            single_tag = eth / vlan2 / ip / udp / payload
+            double_tag = eth / vlan1 / vlan2 / ip / udp / payload
+            return single_tag, double_tag
+
+        def gen_untagged_packet():
+            eth = scapy.Ether(src='02:1e:67:9f:4d:ae', dst='06:16:3e:1b:72:32')
+            ip = scapy.IP()
+            udp = scapy.UDP(sport=10001, dport=10002)
+            payload = 'truculence'
+            pkt = eth / ip / udp / payload
+            return pkt
+
+        expected = []
+
+        for vid in vids:
+            p, pp = gen_packet(vid)
+            q = gen_untagged_packet()
+            if vid >= 0:
+                if not double_tag:
+                    expected.append({
+                        'input_port': 0,
+                        'output_port': vid,
+                        'input_packet': p,
+                        'output_packet': q})
+                else:
+                    expected.append({
+                        'input_port': 0,
+                        'output_port': vid,
+                        'input_packet': pp,
+                        'output_packet': p})
+            else:
+                expected.append({
+                    'input_port': 0,
+                    'output_port': default_gate,
+                    'input_packet': q,
+                    'output_packet': q})
+        return [VLANSplit(), 1, 150, expected]
+
+    OUTPUT_TEST_INPUTS.append(output_test([1, 100, 77, -1, 149, 50, 100, -1]))
+    OUTPUT_TEST_INPUTS.append(output_test([100, 77, -1, 149, 50, 100, -1, 33, 70]))
+    OUTPUT_TEST_INPUTS.append(output_test([100, 77, -1, 149, 50, 100, -1, 33, 70], True))
