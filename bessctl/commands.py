NetSys/bess

commit 4e7c583fb29eee30692eea2270edba0e8708ba48

Prevent `bessctl` crash from  bess config file loading errors config

bessctl/commands.py
@@ -784,11 +784,11 @@ def _get_bess_module_and_port_creators(cli, rsvd):
 
 # NOTE: the name of this function is used below
 def _do_run_file(cli, conf_file):
-    if not os.path.exists(conf_file):
-        cli.err('Cannot open file "%s"' % conf_file)
+    try:
+        xformed = sugar.xform_file(conf_file)
+    except (IOError, OSError):
+        cli.err('Cannot open file %s' % conf_file)
         return
-
-    xformed = sugar.xform_file(conf_file)
 
     new_globals = {
         '__builtins__': __builtins__,
@@ -809,7 +809,12 @@ def _do_run_file(cli, conf_file):
     for name in creators:
         new_globals[name] = creators[name]
 
-    code = compile(xformed, conf_file, 'exec')
+    try:
+        code = compile(xformed, conf_file, 'exec')
+    except:
+        # TODO: Provide more information where and why fail to compile
+        cli.err('Fail to compile bess config file %s ' % conf_file)
+        return
 
     if is_pipeline_empty(cli):
         cli.bess.pause_all()

commit 220bb114e6197b251da289eab3af8aa3bdd96f63

bessctl: Report error location when loading a bess script fails
Location reporting disappeared with commit 4e7c583fb29ee

bessctl/commands.py
@@ -833,9 +833,16 @@ def _do_run_file(cli, conf_file):
 
     try:
         code = compile(xformed, conf_file, 'exec')
-    except:
-        # TODO: Provide more information where and why fail to compile
-        cli.err('Fail to compile bess config file %s ' % conf_file)
+    except SyntaxError as e:
+        # TODO: e.offset might be wrong if there's a correct syntactic
+        #       sugar in an erroneous line
+
+        # Mimic python's error reporting style
+        cli.err('\n  File "%s", line %d\n    %s\n    %s\nSyntaxError: %s' %
+                (conf_file, e.lineno, e.text, ' '*(e.offset - 1) + '^', e.msg))
+        raise cli.HandledError()
+    except Exception as e:
+        cli.err('Fail to compile bess config file (%s): %s ' % (conf_file, e))
         raise cli.HandledError()
 
     if is_pipeline_empty(cli):